66889925686d53c3092878ebe6205ebd1a7c3fe6
pushtakio <alex@vaiolabs.com>
2019-07-09 20:32:21 +0300

    adding travis file

--- setup.py
+++ setup.py
@@ -43,5 +43,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
